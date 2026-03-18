7056000fe4cf14a324895aa891a4851cf3eedfe3
Aubrie Hanna <aubriehanna@Mac.lan>
2026-03-18 16:35:15 -0500

    Added Flavor Finder Page

---
+++ src/pages/FlavorFinder.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+import { Card } from 'react-bootstrap'
+
+export default function FlavorFinder() {
+  return (
+    <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+      <h1 style={{ textAlign: "center"}}>FlavorFinder</h1>
+    </div>
+  )
+}
